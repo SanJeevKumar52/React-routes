2cc3c446aed6e214942415010a7456a91b524621
Sanjeev <sk3616593@gmail.com>
2024-01-07 01:43:03 +0530

    Learning raect router

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Items from "./pages/Items";
+import Navbar from "./components/Navbar";
+
+function App() {
+
+
+
+  const router = createBrowserRouter([
+
+    {
+      path: '/',
+      element: <Navbar />,
+      children: [
+        { index :true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "items", element: <Items /> }
+      ],
+     },
+  ]);
+
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
